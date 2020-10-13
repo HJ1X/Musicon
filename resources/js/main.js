af16bf2325e1c9c6cf3dd731f1be4c210c13ca32
HJ1X <54548986+HJ1X@users.noreply.github.com>
2020-10-13 21:23:24 +0530

    add images

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,8 +4,7 @@ const context = {
     "Musicon is a budding musical storefront with a mission to share the joy of music. These magnificent auditory tools are designed with musical creators, like you, in mind. Hobbyists, beginners, and experts alike can appreciate the resplendent sounds supplied by each and every instrument we carry. Join us in delivering the euphoric vibrations of melodia to the citizens of the world!",
   instruments: [
     {
-      image:
-        "https://content.codecademy.com/courses/learn-handlebars/musicon/electronic-keyboard.png",
+      image: "./resources/images/electronic-keyboard.webp",
       name: "Electronic Keyboard",
       description:
         "A piano welcomed to the 21th century. Pianists celebrate the compact form factor and the diversity of synthesized rhythms all in one masterful musical machine.",
@@ -13,24 +12,21 @@ const context = {
       sale: "$1,699.89",
     },
     {
-      image:
-        "https://content.codecademy.com/courses/learn-handlebars/musicon/electric-guitar.png",
+      image: "./resources/images/electric-guitar.png",
       name: "Electric Guitar",
       description:
         "Join the legends of the '50s and '60s when the marriage of guitar and electricity created the most influential instrument of a generation. Note: picks sold separately.",
       price: "$599.99",
     },
     {
-      image:
-        "https://content.codecademy.com/courses/learn-handlebars/musicon/bass-guitar.png",
+      image: "./resources/images/bass-guitar.webp",
       name: "Bass Guitar",
       description:
         "Experience the embodiment of funkadelic frequencies that is the bass guitar. Let the deep low notes of the bass guitar resonate with heartbeats everywhere.",
       price: "$624.99",
     },
     {
-      image:
-        "https://content.codecademy.com/courses/learn-handlebars/musicon/drum-kit.png",
+      image: "./resources/images/drum-kit.png",
       name: "Drum Kit",
       description:
         'Ever thought, "one instrument isn\'t enough?" Find an answer in the drum kit. Coordinate a collections of drums and cymbals to dictate the rhythm of musical masterpiece.',
@@ -38,8 +34,7 @@ const context = {
       sale: "$349.00",
     },
     {
-      image:
-        "https://content.codecademy.com/courses/learn-handlebars/musicon/violin.png",
+      image: "./resources/images/violin.png",
       name: "Violin",
       description:
         "A versatile that is suited for any and all occasions. Those wearing tuxedos can strum together a classic. Others who prefer overalls can call it a fiddle and play some folk songs.",
